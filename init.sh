daf2d14e12f819b54dec0d72df0246537b09ee64
Chase Dillard <ckdillard@localhost.localdomain>
2022-02-17 15:48:12 -0500

    Add vim-plug install

--- init.sh
+++ init.sh
@@ -5,4 +5,7 @@ mkdir $HOME/.config
 git clone https://github.com/Risa-G/vim-config.git $HOME/.config/nvim
 cp $HOME/.config/nvim/init.vim $HOME/.vimrc
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 nvim -c "+PlugInstall"
