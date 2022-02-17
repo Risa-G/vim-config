e4f4cfab0924f53a591ec9912c37f2861e377b24
Chase Dillard <ckdillard@localhost.localdomain>
2022-02-17 15:40:49 -0500

    Add vim-plug installation

--- init.sh
+++ init.sh
@@ -5,4 +5,7 @@ mkdir $HOME/.config
 git clone https://github.com/Risa-G/vim-config.git $HOME/.config/nvim
 cp $HOME/.config/nvim/init.vim $HOME/.vimrc
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 vim "+PlugInstall" "+qa"
