b28970bfa76cf84497ad56ea2047e4b3acaefc78
Chase Dillard <ckdillard@localhost.localdomain>
2022-02-17 15:47:43 -0500

    Remove vim compat

--- init.sh
+++ init.sh
@@ -5,7 +5,4 @@ mkdir $HOME/.config
 git clone https://github.com/Risa-G/vim-config.git $HOME/.config/nvim
 cp $HOME/.config/nvim/init.vim $HOME/.vimrc
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-vim -c "+PlugInstall" "+qa"
+nvim -c "+PlugInstall"
